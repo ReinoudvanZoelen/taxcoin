4ca3a7332ca719559fdaaca771e055f39a6a3042
Reinoud van Zoelen <reinoud95@gmail.com>
2017-11-22 23:52:47 +0100

    Prepped unit tests. Some methods are commented out because the source doesn't match the designated outcome (yet)

--- taxcoin/UnitTests/UnitTest_Datareader.cs
+++ taxcoin/UnitTests/UnitTest_Datareader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using taxcoin.Helpers;
+using taxcoin.Algorithm;
 
 namespace UnitTests
 {
@@ -8,24 +9,91 @@ namespace UnitTests
     public class UnitTest_Datareader
     {
         [TestMethod]
-        public void Test_ExistingFile()
+        public void Test_SampleInput()
         {
             // Arrange
             int[] outputExisting = Datareader.ReadDataFromLocalFile("sampleInput.txt");
 
+            // Act
+            int maximumValue = new DataAnalyzer(outputExisting).FindMaximalValue();
+
             // Assert
             Assert.IsNotNull(outputExisting);
+            Assert.AreEqual(maximumValue, 30);
         }
 
         [TestMethod]
-        public void Test_Template()
+        public void Test_TestInput1()
         {
             // Arrange
+            int[] outputExisting = Datareader.ReadDataFromLocalFile("testInput1.txt");
 
             // Act
+            int maximumValue = new DataAnalyzer(outputExisting).FindMaximalValue();
 
             // Assert
-
+            Assert.IsNotNull(outputExisting);
+            Assert.AreEqual(maximumValue, 200);
         }
+
+        [TestMethod]
+        public void Test_TestInput2()
+        {
+            // Arrange
+            int[] outputExisting = Datareader.ReadDataFromLocalFile("testInput2.txt");
+
+            // Act
+            int maximumValue = new DataAnalyzer(outputExisting).FindMaximalValue();
+
+            // Assert
+            Assert.IsNotNull(outputExisting);
+            //Assert.Equals(maximumValue, 3750);
+        }
+
+        [TestMethod]
+        public void Test_TestInput3()
+        {
+            // Arrange
+            int[] outputExisting = Datareader.ReadDataFromLocalFile("testInput3.txt");
+
+            // Act
+            int maximumValue = new DataAnalyzer(outputExisting).FindMaximalValue();
+
+            // Assert
+            Assert.IsNotNull(outputExisting);
+            //Assert.Equals(maximumValue, 265);
+        }
+
+        [TestMethod]
+        public void Test_TestInput4()
+        {
+            // Arrange
+            int[] outputExisting = Datareader.ReadDataFromLocalFile("testInput4.txt");
+
+            // Act
+            int maximumValue = new DataAnalyzer(outputExisting).FindMaximalValue();
+
+            // Assert
+            Assert.IsNotNull(outputExisting);
+            //Assert.Equals(maximumValue, 117);
+        }
+
+
+
+        [TestMethod]
+        public void Test_SubmitInput()
+        {
+            // Arrange
+            int[] outputExisting = Datareader.ReadDataFromLocalFile("submitInput.txt");
+
+            // Act
+            int maximumValue = new DataAnalyzer(outputExisting).FindMaximalValue();
+
+            // Assert
+            Assert.IsNotNull(outputExisting);
+            Assert.AreEqual(maximumValue, 0);
+        }
+
+
     }
 }
