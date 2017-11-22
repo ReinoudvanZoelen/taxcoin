0be8e72d00d24c6641a0ddd34feb60a3740b66fb
Reinoud van Zoelen <reinoud95@gmail.com>
2017-11-22 20:12:16 +0100

    Added the unit test solution

---
+++ taxcoin/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3eeff9a-f00e-41af-9cb3-0424b1f4e612")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ taxcoin/UnitTests/UnitTest_Datareader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using taxcoin.Helpers;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest_Datareader
+    {
+        [TestMethod]
+        public void Test_ExistingFile()
+        {
+            // Arrange
+            int[] outputExisting = Datareader.ReadDataFromLocalFile("sampleInput.txt");
+
+            // Assert
+            Assert.IsNotNull(outputExisting);
+        }
+
+        [TestMethod]
+        public void Test_Template()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
