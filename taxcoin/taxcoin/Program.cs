198511083a1fbb55b9cca7b09f5e39440318e676
Reinoud van Zoelen <reinoud95@gmail.com>
2017-11-23 22:24:49 +0100

    Removed reading of a test file that was removed

--- taxcoin/taxcoin/Program.cs
+++ taxcoin/taxcoin/Program.cs
@@ -7,16 +7,15 @@ namespace taxcoin
     {
         static void Main(string[] args)
         {
-            int[][] datasets = new int[6][];
+            int[][] datasets = new int[5][];
 
             datasets[0] = Datareader.ReadDataFromLocalFile("testInput1.txt");
             datasets[1] = Datareader.ReadDataFromLocalFile("sampleInput.txt");
             datasets[2] = Datareader.ReadDataFromLocalFile("submitInput.txt");
             datasets[3] = Datareader.ReadDataFromLocalFile("testInput2.txt");
             datasets[4] = Datareader.ReadDataFromLocalFile("submitInput.txt");
-            datasets[5] = Datareader.ReadDataFromLocalFile("submitInputEdited.txt");
 
-            int endBudget = new DataAnalyzer(datasets[5]).CalculateOptimalRevenue();
+            int endBudget = new DataAnalyzer(datasets[0]).CalculateOptimalRevenue();
 
             Console.WriteLine("");
             Console.WriteLine("No new means of earning money have been found. Exiting algorithm with " + endBudget + " euros");
